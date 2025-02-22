46da985766af3d0ef6a6b1d7841fff60c277699d
Felipe Fontoura <ffontouras@gmail.com>
2025-02-22 01:17:17 -0300

    formatting on save

--- nvim/.config/nvim/lua/plugins/coding.formatting.lua
+++ nvim/.config/nvim/lua/plugins/coding.formatting.lua
@@ -3,6 +3,7 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
       local null_ls = require("null-ls")
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
       null_ls.setup({
         sources = {
@@ -22,6 +23,25 @@ return {
           null_ls.builtins.formatting.rubocop,
           null_ls.builtins.formatting.erb_lint,
         },
+
+        -- Format on save
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  bufnr = bufnr,
+                  filter = function(c)
+                    return c.name == "null-ls"
+                  end
+                })
+              end,
+            })
+          end
+        end,
       })
     end,
   },
