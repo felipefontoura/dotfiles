27ca22ebbe384d58f0fa8cd0a5c0fb6129b6b4a8
Felipe Fontoura <ffontouras@gmail.com>
2025-02-25 09:51:13 -0300

    add mason-installer as dependency

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,44 +1,44 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    opts = {
-      auto_update = true,
-      -- ensure_installed = {
-      --   "bash-language-server",
-      -- },
-    },
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    lazy = false,
-    opts = {
-      auto_update = true,
-      ensure_installed = {
-        "bash-language-server",
-        "black",
-        "css-variables-language-server",
-        "css-lsp",
-        "erb-lint",
-        "eslint_d",
-        "harper-ls",
-        "html-lsp",
-        "isort",
-        "json-lsp",
-        "lua-language-server",
-        "prettier",
-        "python-lsp-server",
-        "rubocop",
-        "ruby-lsp",
-        "stimulus-language-server",
-        "stylua",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "yaml-language-server",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		lazy = false,
+		opts = {
+			auto_update = true,
+		},
+		config = function()
+			require("mason").setup()
+
+			require("mason-tool-installer").setup({
+				auto_update = true,
+				ensure_installed = {
+					"bash-language-server",
+					"black",
+					"css-variables-language-server",
+					"css-lsp",
+					"erb-formatter",
+					"erb-lint",
+					"eslint_d",
+					"harper-ls",
+					"html-lsp",
+					"isort",
+					"json-lsp",
+					"lua-language-server",
+					"markdownlint-cli2",
+					"prettier",
+					"python-lsp-server",
+					"rubocop",
+					"ruby-lsp",
+					"stimulus-language-server",
+					"stylua",
+					"tailwindcss-language-server",
+					"typescript-language-server",
+					"yamlfmt",
+					"yaml-language-server",
+				},
+			})
+		end,
+	},
 }
