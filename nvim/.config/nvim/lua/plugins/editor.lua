9d3b80b1fbe84f48445a63c7943e1319fca72b6d
Felipe Fontoura <ffontouras@gmail.com>
2025-02-26 09:40:50 -0300

    hidding specific files/dirs.

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -6,7 +6,6 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
-			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		lazy = false,
 		opts = {
@@ -17,6 +16,11 @@ return {
 					hide_gitignored = true,
 					never_show = {
 						"node_modules",
+						".git",
+						".github",
+						".kamal",
+						".ruby-lsp",
+						".venv",
 						".DS_Store",
 					},
 				},
