87937a625c72084fe69b7f21922b1e651a0531ba
Felipe Fontoura <ffontouras@gmail.com>
2025-03-31 08:10:35 -0300

    enable pt-br spell check

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Add Portuguese (Brazil) to spell checking languages
+vim.cmd("set spelllang=en,pt,pt_br")
+vim.cmd("set spell")
+
+-- Enable spell checking for markdown and text files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
