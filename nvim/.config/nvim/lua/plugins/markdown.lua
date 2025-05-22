e61a5d6bc44c8742b4cf779aaca29863729d120d
Felipe Fontoura <ffontouras@gmail.com>
2025-05-22 15:27:27 -0300

    drop markdown lint

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,13 @@
+-- if true then return {} end
+
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+}
