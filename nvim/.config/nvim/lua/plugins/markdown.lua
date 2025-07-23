f769b00a9c3bc138878e2140c495ab3da64a59d6
Felipe Fontoura <ffontouras@gmail.com>
2025-07-23 13:25:54 -0300

    adjust display and nvim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -10,4 +10,13 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = {},
+      },
+    },
+  },
 }
